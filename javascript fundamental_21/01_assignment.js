b51794acf7f648816dfd8add196a0e8e2e142bc8
harshitdi78 <146644296+harshitdi78@users.noreply.github.com>
2024-04-14 12:54:48 +0530

    Add files via upload
    assignment files are added

---
+++ javascript fundamental_21/01_assignment.js
@@ -0,0 +1,18 @@
+
+function outerFunction(parameter) {
+
+   let localvar = 'I am Ayush variable';
+
+    function innerFunction() { 
+
+        console.log('Parameter of outerFunction:', parameter); 
+        console.log('Local variable of outerFunction:', localvar);
+    }
+
+   return innerFunction;
+}
+
+const innerFunc = outerFunction('Ayush');
+
+innerFunc()
+
